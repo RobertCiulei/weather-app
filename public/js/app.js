e10f318454796e514d2900a82b9825a2e4be046e
Robert Alexandru <robert.alexandru96@gmail.com>
2022-04-18 13:32:53 +0300

    Prepared project for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,7 @@
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-fetch('http://localhost:3000/weather?address=Bucharest').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.forecast)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -29,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
@@ -39,6 +21,5 @@ weatherForm.addEventListener('submit', (e) => {
             }
             
         })
-    })
-    
+    }) 
 })
